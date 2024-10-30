9526d4953e2363076fde35307162ae4c7ae9292d
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-10-31 00:56:57 +0530

    update db

---
+++ prisma/migrations/20241030192422_add_contact_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "contact" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "subject" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "contact_pkey" PRIMARY KEY ("id")
+);
