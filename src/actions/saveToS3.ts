36488e0e3ded2a4ac17771c6241d895a5702b998
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-11-07 22:57:31 +0530

    add try catch

--- src/actions/saveToS3.ts
+++ src/actions/saveToS3.ts
@@ -5,44 +5,50 @@ import { saveToDb } from "./saveToDb";
 const CLOUDFRONT_URL = process.env.NEXT_PUBLIC_CLOUDFRONT_URL;
 
 export async function saveToS3(formData: FormData, presignedUrl: string, fileName: string, audioType: string, transactionId: string) {
-    console.log("Inside saveToS3...");
-    console.log("formData: ", formData);
-    console.log("presignedUrl: ", presignedUrl);
-    console.log("fileName: ", fileName);
-    console.log("audioType: ", audioType);
-    console.log("transactionId: ", transactionId);
+    try {
+        console.log("Inside saveToS3...");
+        console.log("formData: ", formData);
+        console.log("presignedUrl: ", presignedUrl);
+        console.log("fileName: ", fileName);
+        console.log("audioType: ", audioType);
+        console.log("transactionId: ", transactionId);
 
-    console.log("Fetching presignedUrl...");
-    const response = await fetch(presignedUrl, {
-        method: "POST",
-        body: formData,
-    });
+        console.log("Fetching presignedUrl...");
+        const response = await fetch(presignedUrl, {
+            method: "POST",
+            body: formData,
+        });
 
-    console.log("S3 Upload Response: ", response);
+        console.log("S3 Upload Response: ", response);
 
-    if (response.ok) {
-        const key = formData.get("key") || "unknown";
-        console.log("Key: ", key);
+        if (response.ok) {
+            const key = formData.get("key") || "unknown";
+            console.log("Key: ", key);
 
-        const fileContentType = formData.get("Content-Type") || "unknown";
-        const url = await `${CLOUDFRONT_URL}/${key}`;
+            const fileContentType = formData.get("Content-Type") || "unknown";
+            const url = `${CLOUDFRONT_URL}/${key}`;
 
-        // Save file to database with payment status as pending
-        const dbSave = await saveToDb(url, `${fileName}`, `${fileContentType}`, audioType, key.toString(), transactionId);
+            // Save file to database with payment status as pending
+            const dbSave = await saveToDb(url, `${fileName}`, `${fileContentType}`, audioType, key.toString(), transactionId);
 
-        if (dbSave === null) {
-            console.error("Failed to save file to database");
+            if (dbSave === null) {
+                console.error("Failed to save file to database");
+                return false;
+            }
+
+            if (dbSave !== null) {
+                console.log("File saved to database");
+                console.log("Transcription job awaited");
+            }
+
+            console.log("Uploaded Url: ", formData.get("key"));
+            return true;
+        } else {
+            console.error("S3 Upload Error:", response);
             return false;
         }
-        if (dbSave !== null) {
-            console.log("File saved to database");
-            console.log("Transcription job awaited");
-        }
-        console.log("Uploaded Url: ", formData.get("key"));
-
-        return true;
-    } else {
-        console.error("S3 Upload Error:", response);
+    } catch (error) {
+        console.error("Error in saveToS3:", error);
         return false;
     }
 }
